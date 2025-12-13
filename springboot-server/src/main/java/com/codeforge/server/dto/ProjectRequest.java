570f5e90286b6f3ed9b8edc75c68d0d66348b054
Lokesh S <lokesh.sinduluri@gmail.com>
2025-12-13 16:46:27 +0530

    chore: remove Django and Spring Boot services and their PostgreSQL dependencies

--- springboot-server/src/main/java/com/codeforge/server/dto/ProjectRequest.java
+++
@@ -1,9 +0,0 @@
-package com.codeforge.server.dto;
-
-import lombok.Data;
-
-@Data
-public class ProjectRequest {
-    private String name;
-    private String description;
-}
--- springboot-server/src/main/java/com/codeforge/server/repository/ProjectFileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codeforge.server.repository;
-
-import com.codeforge.server.model.ProjectFile;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProjectFileRepository extends JpaRepository<ProjectFile, String> {
-}
